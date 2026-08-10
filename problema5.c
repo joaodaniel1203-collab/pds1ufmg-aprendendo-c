9443c357a0d70278c78d49f9b329ea4a3e7c25ca
João Daniel <joaodaniel1203@gmail.com>
2026-08-10 13:29:17 -0300

    1.3

---
+++ problema5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void){
+    int a1, b1, c1;
+    int a, b, c;
+    int inteiros;
+
+    printf("Digite três numeros inteiros: ");
+    scanf("%d %d %d", &a1, &b1, &c1);
+
+    a = a1 * a1;
+    b = b1 * b1;
+    c = c1 * c1;
+    inteiros = a + b + c;
+
+    printf("A soma dos quadrados dos numeros digitados e: %d\n", inteiros);
+    return 0;
+}
